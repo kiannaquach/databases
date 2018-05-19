65a70b2c023dc7b2442f4872d1b3a2dd82db211f
kiannaquach <kiannaquach.kq@gmail.com>
2018-05-18 21:17:31 -0700

    Clean-up code|Set up controllers for /classes/messages

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,8 +2,18 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      // Right now this is just a basic response
+      // Will need to change this to a response with data from the db
+      console.log('The request got to messages.get');
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end();
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      console.log('The request got to messages.post');
+      res.writeHead(201, {'Content-Type': 'application/json'});
+      res.end();
+    } // a function which handles posting a message to the database
   },
 
   users: {
--- server/models/index.js
+++ server/models/index.js
@@ -1,20 +1,12 @@
 var db = require('../db');
-// db.addDada, remove
+// Darwin - db.addDada, remove
 // create connection between mySql to the server
 // in db folder
 
 module.exports = {
   messages: {
-    get: function (req, res) {
-      // use db.query(key words and location from db, callback)
-      db.query('INSERT ')
-    }, // a function which produces all the messages
-    post: function (req, res) {
-      db.query('SELECT * FROM messages', function(err, rows, fields) {
-        if err throw err;
-        
-      })
-    } // a function which can be used to insert a message into the database
+    get: function (req, res) {}, // a function which produces all the messages
+    post: function (req, res) {} // a function which can be used to insert a message into the database
   },
 
   users: {
